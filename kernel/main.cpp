888f3ab2d801f2a9b630bb28da0daf33fe647f73
m0704 <naoshimurata8@gmail.com>
2024-01-15 20:12:41 +0900

    osbook_day03c

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,3 +1,12 @@
-extern "C" void KernelMain() {
+#include <cstdint>
+
+extern "C" void KernelMain(uint64_t frame_buffer_base,
+                           uint64_t frame_buffer_size) {
+    uint8_t* frame_buffer = reinterpret_cast<uint8_t*>(frame_buffer_base);
+    // frame_buffer ポインタ に frame_buffer_base ポインタ を格納する
+
+    for (uint64_t i = 0; i < frame_buffer_size; ++i) {
+        frame_buffer[i] = i % 256;
+    }
     while (1) __asm__("hlt");
 }
