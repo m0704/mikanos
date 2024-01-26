de2bab7eb0110489601df5bf32500c69b60e769a
m0704 <naoshimurata8@gmail.com>
2024-01-26 21:19:37 +0900

    day05c

---
+++ kernel/.main.d
@@ -0,0 +1,6 @@
+main.o: main.cpp /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  frame_buffer_config.hpp graphics.hpp font.hpp
