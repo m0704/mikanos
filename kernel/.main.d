23db91335bd8e3c5bc844485bcefe195e23701ba
m0704 <naoshimurata8@gmail.com>
2024-01-28 21:44:36 +0900

    day05d

--- kernel/.main.d
+++ kernel/.main.d
@@ -3,4 +3,6 @@ main.o: main.cpp /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
   /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
   /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
   /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
   frame_buffer_config.hpp graphics.hpp font.hpp
