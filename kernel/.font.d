273476738491a4137e9f15f804e1d6b4e0c66567
m0704 <naoshimurata8@gmail.com>
2024-02-03 21:27:18 +0900

    day06a

--- kernel/.font.d
+++
@@ -1,5 +0,0 @@
-font.o: font.cpp font.hpp \
-  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
-  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/__config \
-  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h graphics.hpp \
-  frame_buffer_config.hpp
--- kernel/.main.d
+++
@@ -1,8 +0,0 @@
-main.o: main.cpp /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
-  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/__config \
-  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
-  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
-  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/version \
-  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
-  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
-  frame_buffer_config.hpp graphics.hpp font.hpp console.hpp
