aed9db920a017081cc1f87ec242509989fac75f0
m0704 <naoshimurata8@gmail.com>
2024-03-09 22:03:04 +0900

    day08c

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -27,6 +27,9 @@
 #include "interrupt.hpp"
 #include "asmfunc.h"
 #include "queue.hpp"
+#include "segment.hpp"
+#include "paging.hpp"
+#include "memory_manager.hpp"
 
 const PixelColor kDesktopBGColor{45, 118, 237};
 const PixelColor kDesktopFGColor{255, 255, 255};
@@ -49,6 +52,11 @@ int printk(const char* format, ...) {
   console->PutString(s);
   return result;
 }
+
+// #@@range_begin(memman_buf)
+char memory_manager_buf[sizeof(BitmapMemoryManager)];
+BitmapMemoryManager* memory_manager;
+// #@@range_end(memman_buf)
 
 char mouse_cursor_buf[sizeof(MouseCursor)];
 MouseCursor* mouse_cursor;
@@ -78,7 +86,6 @@ void SwitchEhci2Xhci(const pci::Device& xhc_dev) {
       superspeed_ports, ehci2xhci_ports);
 }
 
-// #@@range_begin(xhci_handler)
 usb::xhci::Controller* xhc;
 
 struct Message {
@@ -94,9 +101,15 @@ void IntHandlerXHCI(InterruptFrame* frame) {
   main_queue->Push(Message{Message::kInterruptXHCI});
   NotifyEndOfInterrupt();
 }
-// #@@range_end(xhci_handler)
 
-extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config, const MemoryMap& memory_map) {
+alignas(16) uint8_t kernel_main_stack[1024 * 1024];
+
+extern "C" void KernelMainNewStack(
+    const FrameBufferConfig& frame_buffer_config_ref,
+    const MemoryMap& memory_map_ref) {
+  FrameBufferConfig frame_buffer_config{frame_buffer_config_ref};
+  MemoryMap memory_map{memory_map_ref};
+
   switch (frame_buffer_config.pixel_format) {
     case kPixelRGBResv8BitPerColor:
       pixel_writer = new(pixel_writer_buf)
@@ -134,28 +147,42 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config, const M
   printk("Welcome to MikanOS!\n");
   SetLogLevel(kWarn);
 
-  const std::array available_memory_types{
-    MemoryType::kEfiBootServicesCode,
-    MemoryType::kEfiBootServicesData,
-    MemoryType::kEfiConventionalMemory,
-  };
+  SetupSegments();
 
-  printk("memory_map: %p\n", &memory_map);
-  for (uintptr_t iter = reinterpret_cast<uintptr_t>(memory_map.buffer);
-      iter < reinterpret_cast<uintptr_t>(memory_map.buffer) + memory_map.map_size;
-      iter += memory_map.descriptor_size) {
-        auto desc = reinterpret_cast<MemoryDescriptor*>(iter);
-        for (int i = 0; i < available_memory_types.size(); ++i) {
-          if (desc->type == available_memory_types[i]) {
-            printk("type = %u, phys = %08lx - %08lx, pages = %lu, attr = %08lx\n",
-              desc->type,
-              desc->physical_start,
-              desc->physical_start + desc->number_of_pages * 4096 - 1,
-              desc->number_of_pages,
-              desc->attribute);
-          }
-        }
-      }
+  const uint16_t kernel_cs = 1 << 3;
+  const uint16_t kernel_ss = 2 << 3;
+  SetDSAll(0);
+  SetCSSS(kernel_cs, kernel_ss);
+
+  SetupIdentityPageTable();
+
+  // #@@range_begin(mark_allocated)
+  ::memory_manager = new(memory_manager_buf) BitmapMemoryManager;
+
+  const auto memory_map_base = reinterpret_cast<uintptr_t>(memory_map.buffer);
+  uintptr_t available_end = 0;
+  for (uintptr_t iter = memory_map_base;
+       iter < memory_map_base + memory_map.map_size;
+       iter += memory_map.descriptor_size) {
+    auto desc = reinterpret_cast<const MemoryDescriptor*>(iter);
+    if (available_end < desc->physical_start) {
+      memory_manager->MarkAllocated(
+          FrameID{available_end / kBytesPerFrame},
+          (desc->physical_start - available_end) / kBytesPerFrame);
+    }
+
+    const auto physical_end =
+      desc->physical_start + desc->number_of_pages * kUEFIPageSize;
+    if (IsAvailable(static_cast<MemoryType>(desc->type))) {
+      available_end = physical_end;
+    } else {
+      memory_manager->MarkAllocated(
+          FrameID{desc->physical_start / kBytesPerFrame},
+          desc->number_of_pages * kUEFIPageSize / kBytesPerFrame);
+    }
+  }
+  memory_manager->SetMemoryRange(FrameID{1}, FrameID{available_end / kBytesPerFrame});
+  // #@@range_end(mark_allocated)
 
   mouse_cursor = new(mouse_cursor_buf) MouseCursor{
     pixel_writer, kDesktopBGColor, {300, 200}
@@ -194,21 +221,16 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config, const M
         xhc_dev->bus, xhc_dev->device, xhc_dev->function);
   }
 
-  // #@@range_begin(load_idt)
-  const uint16_t cs = GetCS();
   SetIDTEntry(idt[InterruptVector::kXHCI], MakeIDTAttr(DescriptorType::kInterruptGate, 0),
-              reinterpret_cast<uint64_t>(IntHandlerXHCI), cs);
+              reinterpret_cast<uint64_t>(IntHandlerXHCI), kernel_cs);
   LoadIDT(sizeof(idt) - 1, reinterpret_cast<uintptr_t>(&idt[0]));
-  // #@@range_end(load_idt)
 
-  // #@@range_begin(configure_msi)
   const uint8_t bsp_local_apic_id =
     *reinterpret_cast<const uint32_t*>(0xfee00020) >> 24;
   pci::ConfigureMSIFixedDestination(
       *xhc_dev, bsp_local_apic_id,
       pci::MSITriggerMode::kLevel, pci::MSIDeliveryMode::kFixed,
       InterruptVector::kXHCI, 0);
-  // #@@range_end(configure_msi)
 
   const WithError<uint64_t> xhc_bar = pci::ReadBar(*xhc_dev, 0);
   Log(kDebug, "ReadBar: %s\n", xhc_bar.error.Name());
@@ -229,7 +251,6 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config, const M
   xhc.Run();
 
   ::xhc = &xhc;
-  __asm__("sti");
 
   usb::HIDMouseDriver::default_observer = MouseObserver;
 
@@ -258,15 +279,16 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config, const M
     __asm__("sti");
 
     switch (msg.type) {
-      case Message::kInterruptXHCI:
-        while (xhc.PrimaryEventRing()->HasFront()) {
-          if (auto err = ProcessEvent(xhc)) {
-            Log(kError, "Error while ProcessEvent: %s at %s:%d\n", err.Name(), err.File(), err.Line());
-          }
+    case Message::kInterruptXHCI:
+      while (xhc.PrimaryEventRing()->HasFront()) {
+        if (auto err = ProcessEvent(xhc)) {
+          Log(kError, "Error while ProcessEvent: %s at %s:%d\n",
+              err.Name(), err.File(), err.Line());
         }
-        break;
-      default:
-        Log(kError, "Unknown message type: %d\n", msg.type);
+      }
+      break;
+    default:
+      Log(kError, "Unknown message type: %d\n", msg.type);
     }
   }
 }
