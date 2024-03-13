e6efeca4cddc827981d08644623c4b3a3e225f39
m0704 <naoshimurata8@gmail.com>
2024-03-13 22:20:57 +0900

    day09a

--- kernel/.console.d
+++ kernel/.console.d
@@ -3,4 +3,40 @@ console.o: console.cpp console.hpp graphics.hpp frame_buffer_config.hpp \
   /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/__config \
   /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstring \
   /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/string.h font.hpp \
-  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstdint
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstdint layer.hpp \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/memory \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/type_traits \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/typeinfo \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/exception \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cstdlib \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/stdlib.h \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/new \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/utility \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/__tuple \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/initializer_list \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/__debug \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/limits \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/iterator \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/iosfwd \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/wchar.h \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/__functional_base \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/tuple \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/stdexcept \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/cassert \
+  /home/sz/osbook/devenv/x86_64-elf/include/assert.h \
+  /home/sz/osbook/devenv/x86_64-elf/include/_ansi.h \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/map \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/__tree \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/algorithm \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/functional \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/bit \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/__node_handle \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/optional \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/vector \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/__bit_reference \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/climits \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/limits.h \
+  /home/sz/osbook/devenv/x86_64-elf/include/c++/v1/__split_buffer \
+  window.hpp
